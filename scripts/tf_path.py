6b529e1491d24acd446db72c4f3019b5e5e4bdd3
Lucas Walter <wsacul@gmail.com>
2021-12-13 10:09:25 -0800

    turn tf path into class object

--- scripts/tf_path.py
+++ scripts/tf_path.py
@@ -11,44 +11,53 @@ import tf2_py as tf2
 import traceback
 
 from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import Path
 
-rospy.init_node('old_tf_to_new_tf')
 
-tf_buffer = tf2_ros.Buffer(cache_time=rospy.Duration(10.0))
-tl = tf2_ros.TransformListener(tf_buffer)
+class TfPath(object):
+    def __init__(self):
+        self.tf_buffer = tf2_ros.Buffer(cache_time=rospy.Duration(10.0))
+        self.tl = tf2_ros.TransformListener(self.tf_buffer)
 
-parent = rospy.get_param("~parent", "map")
-child = rospy.get_param("~child", "base_link")
-update_rate = rospy.get_param("~update_rate", 5.0)
-cache_time = rospy.get_param("~cache_time", 30.0)
+        self.parent = rospy.get_param("~parent", "map")
+        self.child = rospy.get_param("~child", "base_link")
+        self.cache_time = rospy.get_param("~cache_time", 30.0)
 
-pub = rospy.Publisher("path", Path, queue_size=2)
-path = Path()
-path.header.frame_id = parent
+        self.pub = rospy.Publisher("path", Path, queue_size=2)
+        self.path = Path()
+        self.path.header.frame_id = self.parent
 
-while not rospy.is_shutdown():
-    rospy.sleep(1.0 / update_rate)
-    lookup_time = rospy.Time(0)
-    try:
-        trans = tf_buffer.lookup_transform(parent, child, lookup_time)
-    except (tf2.LookupException, tf2.ConnectivityException, tf2.ExtrapolationException) as ex:
-        # TODO(lucasw) only warn on edge
-        rospy.logwarn_throttle(5.0, lookup_time.to_sec())
-        rospy.logwarn_throttle(5.0, traceback.format_exc())
-        continue
+        update_rate = rospy.get_param("~update_rate", 5.0)
+        rospy.loginfo("tf path poses expire after {}s".format(self.cache_time))
+        self.timer = rospy.Timer(rospy.Duration(1.0 / update_rate), self.update)
 
-    pose = PoseStamped()
-    pose.header = trans.header
-    pose.pose.position.x = trans.transform.translation.x
-    pose.pose.position.y = trans.transform.translation.y
-    pose.pose.position.z = trans.transform.translation.z
-    pose.pose.orientation = trans.transform.rotation
-    path.poses.append(pose)
+    def update(self, event):
+        lookup_time = rospy.Time(0)
+        try:
+            tf = self.tf_buffer.lookup_transform(self.parent, self.child, lookup_time)
+        except (tf2.LookupException, tf2.ConnectivityException, tf2.ExtrapolationException) as ex:
+            # TODO(lucasw) only warn on edge
+            rospy.logwarn_throttle(5.0, lookup_time.to_sec())
+            rospy.logdebug_throttle(5.0, traceback.format_exc())
+            rospy.logwarn_throttle(5.0, ex)
+            return
 
-    if (trans.header.stamp - path.poses[0].header.stamp).to_sec() > cache_time:
-        path.poses = path.poses[1:]
+        pose = PoseStamped()
+        pose.header = tf.header
+        pose.pose.position.x = tf.transform.translation.x
+        pose.pose.position.y = tf.transform.translation.y
+        pose.pose.position.z = tf.transform.translation.z
+        pose.pose.orientation = tf.transform.rotation
+        self.path.poses.append(pose)
 
-    path.header.stamp = trans.header.stamp
-    pub.publish(path)
+        if (tf.header.stamp - self.path.poses[0].header.stamp).to_sec() > self.cache_time:
+            self.path.poses = self.path.poses[1:]
+
+        self.path.header.stamp = tf.header.stamp
+        self.pub.publish(self.path)
+
+
+if __name__ == '__main__':
+    rospy.init_node('tf_path')
+    node = TfPath()
+    rospy.spin()
