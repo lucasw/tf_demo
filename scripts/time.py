5d853bab9981950b7cea5a2512ac634697e9da7d
Lucas Walter <wsacul@gmail.com>
2022-05-12 10:37:34 -0700

    need to sleep to get first /clock when /use_sim_time true

--- scripts/time.py
+++ scripts/time.py
@@ -9,10 +9,14 @@ rospy.init_node("time")
 date_format = rospy.get_param("~format", "+%Y%m%d_%H%M%S")
 use_format = rospy.get_param("~use_format", True)
 
+# wait for first timestamp on /clock if /use_sim_time true
+rospy.sleep(rospy.Duration(0.0))
 ts_epoch = rospy.Time.now().to_sec()
 
 if use_format:
     ts = datetime.datetime.fromtimestamp(ts_epoch).strftime(date_format)
+    # TODO(lucasw) what is this '+'?
+    ts = ts.lstrip('+')
 else:
     ts = ts_epoch
 
